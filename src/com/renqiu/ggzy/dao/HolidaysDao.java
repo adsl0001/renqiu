c678ad90e1fefc6bd223db70694a47eb838c2e61
dubz <dubenzun@163.com>
2014-02-10 14:03:34 +0800

    all

---
+++ src/com/renqiu/ggzy/dao/HolidaysDao.java
@@ -0,0 +1,10 @@
+package com.renqiu.ggzy.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.renqiu.ggzy.entity.Holidays;
+
+public interface HolidaysDao extends CrudRepository<Holidays, String> {
+
+	
+}
