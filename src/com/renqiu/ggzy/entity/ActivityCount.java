17d5045b99be0822a0a15115934eabc0d52f0c99
dubz <dubenzun@163.com>
2014-02-14 08:13:58 +0800

    办结率后台

---
+++ src/com/renqiu/ggzy/entity/ActivityCount.java
@@ -0,0 +1,40 @@
+package com.renqiu.ggzy.entity;
+
+public class ActivityCount {
+	 
+	private String gongsang;
+	private String zhijian;
+	private String disui;
+	private String guosui;
+	private String yinzhang;
+	public String getGongsang() {
+		return gongsang;
+	}
+	public void setGongsang(String gongsang) {
+		this.gongsang = gongsang;
+	}
+	public String getZhijian() {
+		return zhijian;
+	}
+	public void setZhijian(String zhijian) {
+		this.zhijian = zhijian;
+	}
+	public String getDisui() {
+		return disui;
+	}
+	public void setDisui(String disui) {
+		this.disui = disui;
+	}
+	public String getGuosui() {
+		return guosui;
+	}
+	public void setGuosui(String guosui) {
+		this.guosui = guosui;
+	}
+	public String getYinzhang() {
+		return yinzhang;
+	}
+	public void setYinzhang(String yinzhang) {
+		this.yinzhang = yinzhang;
+	}
+}
